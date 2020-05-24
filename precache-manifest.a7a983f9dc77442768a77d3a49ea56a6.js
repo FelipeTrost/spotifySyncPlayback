222235940c13ec6b1b440094a1ec47dce7771c98
Felipe Trost <felipe.trost@gmail.com>
2020-05-24 02:57:05 -0400

    Updates

---
+++ precache-manifest.a7a983f9dc77442768a77d3a49ea56a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41495d61eb7a67910ec1831529e129af",
+    "url": "/spotifySyncPlayback/index.html"
+  },
+  {
+    "revision": "21fe17229bbb2da2f060",
+    "url": "/spotifySyncPlayback/static/js/2.a42934e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotifySyncPlayback/static/js/2.a42934e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589c0eb01dfc4156ba92",
+    "url": "/spotifySyncPlayback/static/js/main.da6eb877.chunk.js"
+  },
+  {
+    "revision": "d1c1f9ce0147c9ed19cf",
+    "url": "/spotifySyncPlayback/static/js/runtime-main.282c3973.js"
+  }
+]);
